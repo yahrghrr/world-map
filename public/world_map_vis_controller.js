62615fb64af3801b04ce4ec795f5ce1096348246
yahrghrr <yahrghrr@gmail.com>
2016-11-18 00:31:29 +0800

    Update world_map_vis_controller.js

--- public/world_map_vis_controller.js
+++ public/world_map_vis_controller.js
@@ -4,7 +4,7 @@ define(function (require) {
   let _ = require('lodash');
   const module = require('ui/modules').get('world_map_vis');
 
-  module.controller('KbnHealthMetricVisController', function ($scope, Private) {
+  module.controller('KbnWorldMapVisController', function ($scope, Private) {
     const tabifyAggResponse = Private(require('ui/agg_response/tabify/tabify'));
 
     const metrics = $scope.metrics = [];
